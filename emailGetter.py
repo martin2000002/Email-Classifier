8a1ef169be55d402d77e33e289b195c26b268388
sakash <sakash@vt.edu>
2025-11-24 19:45:32 -0500

    files added

---
+++ emailGetter.py
@@ -0,0 +1,16 @@
+# Sakash Khanna
+
+#calculate value
+def calculate(typeGiven):
+    print(typeGiven)
+    return("scam")
+
+
+def getEmail():
+    #get email
+    emailRecieved="email"
+
+    result = calculate(emailRecieved)
+    
+    # send back to HTML
+    return (result)
